7229a4fae7376a826d4563e06ac15d021bed564a
Nicolas <nick@wireless-10-147-98-249.public.utexas.edu>
2018-02-11 17:13:43 -0600

    added bootstrap template

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-from app import routes
+
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
