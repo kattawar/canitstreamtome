24e910e4529dd3358692acf7b361762ed6751959
Kevin Salcedo <kevsalcedo@gmail.com>
2018-02-11 17:46:51 -0600

    renamed articles.html to movies.html and created countries.html. renamed navbar buttons accordingly

--- app/__init__.py
+++ app/__init__.py
@@ -10,3 +10,9 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+@app.route('/movies')
+def movies():
+    return render_template('movies.html')
+@app.route('/countries')
+def countries():
+    return render_template('countries.html')
