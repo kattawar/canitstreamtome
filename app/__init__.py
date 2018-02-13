43c2a255eceba854c69b2757c60ff072663d6f81
Kevin Salcedo <kevsalcedo@gmail.com>
2018-02-13 11:17:04 -0600

    added links to streaming services instances closes #6

--- app/__init__.py
+++ app/__init__.py
@@ -31,5 +31,15 @@ def movie3():
 @app.route('/movie4')
 def movie4():
     return render_template('movie4.html')
-
-
+@app.route('/service1')
+def service1():
+    return render_template('service1.html')
+@app.route('/service2')
+def service2():
+    return render_template('service2.html')
+@app.route('/service3')
+def service3():
+    return render_template('service3.html')
+@app.route('/service4')
+def service4():
+    return render_template('service4.html')
