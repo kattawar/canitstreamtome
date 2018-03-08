0f869b817223f6eb9dce9dfb010b0f212bc54749
Kevin Salcedo <kevsalcedo@gmail.com>
2018-03-08 15:52:29 -0600

    added paths for countries and services to app routes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import NavBarr from './components/NavBar';
 import {Route, Switch} from 'react-router-dom';
 import Home from './components/Home';
-import Movies from './components/Movies'
-import Bottom from './components/Footer'
+import Movies from './components/Movies';
+import Services from './components/Services';
+import Countries from './components/Countres';
+import Bottom from './components/Footer';
 
 class App extends React.Component {
 
@@ -15,6 +17,8 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/movies" component={Movies} />
+          <Route exact path="/countries" component={Countries} />
+          <Route exact path="/services" component={Services} />
         </Switch>
         <Bottom />
       </div>
