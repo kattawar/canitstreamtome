e7720d5a0e086c5cde2b0deef8161c41d37d3462
kattawar <zach@kattawar.com>
2018-03-09 17:50:11 -0600

    added a simple adapter

--- app/api_adapter.py
+++ app/api_adapter.py
@@ -1,5 +1,10 @@
 import twitter
+import sys
 
+
+
+
+# Verifies twitter api account works
 api = twitter.Api(consumer_key='01cakDvOdyw9ytVgVgA2e1loV',
                       consumer_secret='57Mmg8pz20J8NfFf3cL5QkiFw1gGBDL2nPgf4Dbj7J6daUaXTA',
                       access_token_key='4700491069-O7FycZmg158kJuGLALlLTBJ5CuTmi98mGs5vZ08',
@@ -7,4 +12,23 @@ api = twitter.Api(consumer_key='01cakDvOdyw9ytVgVgA2e1loV',
 
 
 
-print(api.VerifyCredentials())
+
+
+
+if __name__ == "__main__":
+    #print(api.VerifyCredentials())
+    #loop over command line args
+    #used for flipping api scrapers on and off ect...
+    for k in sys.argv:
+      print(k)
+      if k == "twitter":
+        #do twitter
+        #
+      elif k == "guidebox":
+        #do guidebox
+        #
+      elif k == "imdb"
+        #do imdb
+        #
+
+
