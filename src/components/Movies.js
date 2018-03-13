57fc7e644f8bde0bdb2ca03b1b310cfc1cd6dc8a
Nicolas <nicolasjlavigne@gmail.com>
2018-03-13 11:49:12 -0500

    Added axios code

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,10 +4,29 @@ import br from '../img/br.png';
 import toy from '../img/toy.png';
 import tgf from '../img/tgf.png';
 import ww from '../img/ww.png';
+import axios from 'axios';
 
 
 class Movies extends React.Component {
 
+	state = {
+		movieList : []
+	}
+
+	componentDidMount(){
+		axios.get(`https://cors-anywhere.herokuapp.com/http://api.canitstreamto.me/v1/movie?all=true`)
+	       .then(res => {
+	         const movieList = res.data;
+	         this.setState({ movieList });
+					 console.log(movieList);
+	       })
+				  .catch((error) => {
+						console.log(error);
+					});
+
+
+
+	}
 	render() {
 		let vars = [br, toy, tgf, ww, toy, ww, tgf, br, ww, toy, ww, tgf, br, ww, toy, ww, tgf, br, ww, toy, ww, tgf, br, ww, toy, ww, tgf, br, ww, toy, ww, tgf, br];
 
