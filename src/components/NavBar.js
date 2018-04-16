382d7bc6b4eddc1d39f3767fe3024909fa0c0f7a
Kevin Salcedo <kevsalcedo@gmail.com>
2018-04-16 16:15:33 -0500

    small formatting changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,40 +1,38 @@
 import React from 'react';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import {Navbar, Nav, NavItem} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 import SearchBar from './SearchBar.js'
 
 class NavBarr extends React.Component {
-	render() {
-		return (
-       <Navbar inverse collapseOnSelect>
-  <Navbar.Header>
-    <Navbar.Brand>
-      <a href="/">CanItStreamTo.me</a>
-    </Navbar.Brand>
-    <Navbar.Toggle />
-  </Navbar.Header>
-  <Navbar.Collapse>
-    <Nav pullLeft>
-      <NavItem eventKey={1}>
-        <Link to="/movie">Movies</Link>
-      </NavItem>
-      <NavItem eventKey={2}>
-        <Link to="/streaming_service">Streaming Services</Link>
-      </NavItem>
-      <NavItem eventKey={3}>
-        <Link to="/country">Countries</Link>
-      </NavItem>
-      <NavItem eventKey={4}>
-        <Link to="/about">About Us</Link>
-      </NavItem>
-    </Nav>
-    <Navbar.Form pullRight>
-			<SearchBar />
-    </Navbar.Form>
-  </Navbar.Collapse>
-</Navbar>
-    );
-	}
+  render() {
+    return (<Navbar inverse="inverse" collapseOnSelect="collapseOnSelect">
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="/">CanItStreamTo.me</a>
+        </Navbar.Brand>
+        <Navbar.Toggle/>
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav pullLeft="pullLeft">
+          <NavItem eventKey={1}>
+            <Link to="/movie">Movies</Link>
+          </NavItem>
+          <NavItem eventKey={2}>
+            <Link to="/streaming_service">Streaming Services</Link>
+          </NavItem>
+          <NavItem eventKey={3}>
+            <Link to="/country">Countries</Link>
+          </NavItem>
+          <NavItem eventKey={4}>
+            <Link to="/about">About Us</Link>
+          </NavItem>
+        </Nav>
+        <Navbar.Form pullRight="pullRight">
+          <SearchBar/>
+        </Navbar.Form>
+      </Navbar.Collapse>
+    </Navbar>);
+  }
 }
 
 export default NavBarr;
